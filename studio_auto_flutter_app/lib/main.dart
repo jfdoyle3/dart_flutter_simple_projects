096289808e60900d0b81e343a5c921e1add5fcf3
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-03-31 01:31:53 -0400

    added appbar and text

--- studio_auto_flutter_app/lib/main.dart
+++ studio_auto_flutter_app/lib/main.dart
@@ -18,6 +18,12 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text("This is a Flutter App."),
+          centerTitle: true,
+        ),
+    );
   }
 }
